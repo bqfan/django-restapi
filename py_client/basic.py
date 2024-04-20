fc0ca89ba8080c351be410712f93525e3d8d8618
Baiquan Fan <baiquan.fan@gmail.com>
2024-04-20 17:25:38 +0300

    Django Model Instance to Dictionary.

--- py_client/basic.py
+++ py_client/basic.py
@@ -6,6 +6,7 @@ endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World!"})
 
+# print(get_response.headers)
 # print(get_response.text)
 print(get_response.json())
-print(get_response.status_code)
+# print(get_response.status_code)
